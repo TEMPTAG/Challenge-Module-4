6cbf8676d0353f021e34cd5860df850eaf01b43e
Ian Ferguson <iansterlingferguson@gmail.com>
2024-06-28 16:36:29 -0700

    start html and js files, initial test to look like mockups.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,13 @@
 // TODO: Create a variable that selects the form element
+const form = document.querySelector("form");
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
+const formData = new FormData(form);
+const username = formData.get("username");
+const title = formData.get("title");
+const content = formData.get("content");
 
-let redirectURL = '';
+let redirectURL = "blog.html";
 
 const redirectPage = function (url) {
   redirectURL = url;
@@ -10,3 +15,23 @@ const redirectPage = function (url) {
 };
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (!username || !title || !content) {
+    alert("Please complete the form.");
+    return;
+  }
+
+  const blog = {
+    username,
+    title,
+    content,
+  };
+
+  const blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+  blogs.push(blog);
+  localStorage.setItem("blogs", JSON.stringify(blogs));
+
+  redirectPage("blog.html");
+});
