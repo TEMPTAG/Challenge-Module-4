07fdd2fd76c07c6eda2c277f2c4d2bba014a2fed
Ian Ferguson <iansterlingferguson@gmail.com>
2024-06-30 15:39:43 -0700

    starting over blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,35 +2,9 @@
 const main = document.querySelector("main");
 
 // TODO: Create a function that builds an element and appends it to the DOM
-const buildElement = function (element, text, className) {
-  const el = document.createElement(element);
-  el.textContent = text;
-  el.className = className;
-  main.appendChild(el);
-};
 
 // TODO: Create a function that handles the case where there are no blog posts to display
-const noBlogs = function () {
-  buildElement("h3", "No blog posts yet...", "no-blogs");
-};
 
 // TODO: Create a function that reads from local storage and returns the data
-const getBlogs = function () {
-  return JSON.parse(localStorage.getItem("blogs")) || [];
-};
 
 // TODO: Call the function to render the list of blog posts
-const renderBlogs = function () {
-  const blogs = getBlogs();
-
-  if (!blogs.length) {
-    noBlogs();
-    return;
-  }
-
-  blogs.forEach((blog) => {
-    buildElement("h3", blog.title, "title");
-    buildElement("p", `by ${blog.username}`, "username");
-    buildElement("p", blog.content, "content");
-  });
-};
--- assets/js/form.js
+++ assets/js/form.js
@@ -2,22 +2,17 @@
 const form = document.querySelector("form");
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
-// const formData = new FormData(form);
-// const username = formData.get("username");
-// const title = formData.get("title");
-// const content = formData.get("content");
-
 const username = document.querySelector("#username");
 const title = document.querySelector("#title");
 const content = document.querySelector("#content");
 const submitButton = document.querySelector("submit");
 
-// let redirectURL = "blog.html";
+let redirectURL = "blog.html";
 
-// const redirectPage = function (url) {
-//   redirectURL = url;
-//   location.assign(url);
-// };
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
 
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
 form.addEventListener("submit", function (event) {
@@ -33,5 +28,5 @@ form.addEventListener("submit", function (event) {
   blogs.push(blog);
   localStorage.setItem("blogs", JSON.stringify(blogs));
 
-  redirectPage("./blog.html");
+  redirectPage(redirectURL);
 });
