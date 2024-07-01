9a308151cc37c302d9f8cec7122238e8981a1d4b
Ian Ferguson <iansterlingferguson@gmail.com>
2024-07-01 11:33:30 -0700

    toggle work for index functions, now to change circle color.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,19 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+const toggle = document.querySelector("#toggle");
+const header = document.querySelector(".header");
+const circle = document.querySelector(".circle");
+
+let mode = "light";
+
+toggle.addEventListener("click", function () {
+  if (mode === "light") {
+    mode = "dark";
+    header.setAttribute("class", "dark");
+  } else {
+    mode = "light";
+    header.setAttribute("class", "light");
+  }
+  localStorage.setItem("mode", mode);
+});
 
 // TODO: Create functions to read and write from local storage
