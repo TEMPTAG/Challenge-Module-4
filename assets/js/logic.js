38ddc49801d2c776d6326a9d5162f1e2a9ed8533
Ian Ferguson <iansterlingferguson@gmail.com>
2024-07-01 16:32:51 -0700

    functioning js for toggle light/dark modes of page and circle, saves locally, reads/writes from local storage.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,41 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 const toggle = document.querySelector("#toggle");
 const page = document.querySelector(".page");
-const aside = document.querySelector(".aside");
 
-let mode = "light";
+// Mode variable defined, if undefined, set initial mode and save to local storage
+let mode = readMode();
+if (!mode) {
+  mode = "light";
+  saveMode();
+}
+writeMode();
 
+// Event listener to toggle the light/dark modes
 toggle.addEventListener("click", function () {
   if (mode === "light") {
     mode = "dark";
-    page.setAttribute("class", "dark");
-    document.documentElement.style.setProperty("--circle-color", "#000");
   } else {
     mode = "light";
-    page.setAttribute("class", "light");
-    document.documentElement.style.setProperty("--circle-color", "#ffb100");
   }
-  localStorage.setItem("mode", mode);
+  saveMode();
+  writeMode();
 });
 
+function saveMode() {
+  localStorage.setItem("mode", mode);
+}
+
 // TODO: Create functions to read and write from local storage
+
+function readMode() {
+  return localStorage.getItem("mode");
+}
+
+function writeMode() {
+  page.className = "page " + mode;
+  if (mode === "dark") {
+    document.documentElement.style.setProperty("--circle-color", "#000");
+  } else {
+    document.documentElement.style.setProperty("--circle-color", "#ffb100");
+  }
+}
