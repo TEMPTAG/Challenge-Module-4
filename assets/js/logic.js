7ba9dfaa5fd9c8d8c813254cd9c61304abe70553
Ian Ferguson <iansterlingferguson@gmail.com>
2024-07-01 14:06:51 -0700

    got --circle-color to change with toggle.  Now to create functionality per assignment.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,19 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 const toggle = document.querySelector("#toggle");
-const header = document.querySelector(".header");
-const circle = document.querySelector(".circle");
+const page = document.querySelector(".page");
+const aside = document.querySelector(".aside");
 
 let mode = "light";
 
 toggle.addEventListener("click", function () {
   if (mode === "light") {
     mode = "dark";
-    header.setAttribute("class", "dark");
+    page.setAttribute("class", "dark");
+    document.documentElement.style.setProperty("--circle-color", "#000");
   } else {
     mode = "light";
-    header.setAttribute("class", "light");
+    page.setAttribute("class", "light");
+    document.documentElement.style.setProperty("--circle-color", "#ffb100");
   }
   localStorage.setItem("mode", mode);
 });
